07fcf367c79fa1dac32140f9d7336460ae846316
Shangt1145 <tfs33827950@outlook.com>
2026-07-14 21:19:00 +0800

    fix routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import { Routes, Route } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Routes, Route, useNavigate } from 'react-router-dom'
 import Ledger from './pages/Ledger'
 import Console from './pages/Console'
 
 function App() {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const redirect = sessionStorage.getItem('redirect')
+    if (redirect) {
+      sessionStorage.removeItem('redirect')
+      const match = redirect.match(/\/bcma(\/.*)$/)
+      if (match && match[1] && match[1] !== '/') {
+        navigate(match[1], { replace: true })
+      }
+    }
+  }, [navigate])
+
   return (
     <Routes>
       <Route path="/" element={<Ledger />} />
